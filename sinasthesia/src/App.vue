f6c539277cbe70b078bb4b13dd0034905e587cd7
Max Ovchinnkov <max4nn@gmail.com>
2021-01-27 19:29:09 +0300

    initial

---
+++ sinasthesia/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <input v-model="line">
+    <div class="container">
+      <letter v-for="(character, i) in line" :key="i" :character="character"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Letter from './components/Letter.vue'
+export default {
+  name: 'App',
+  components: {
+    Letter
+  },
+  data () {
+    return  {
+      line: '3,1415926'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.container {
+  padding: 20px;
+
+}
+input {
+  position: sticky;
+  top: 50px;
+  left: 28px;
+}
+</style>
---
+++ sinasthesia/src/components/Letter.vue
@@ -0,0 +1,41 @@
+<template>
+    <span :style="{color: color}">{{character}}</span>
+</template>
+
+<script>
+const dictionary = {
+    "0": "#efebe7",
+    "1": "#dedbde",
+    "2": "#6b2821",
+    "3": "#d69608",
+    "4": "#10b6ce",
+    "5": "#b53473",
+    "6": "#215900",
+    "7": "#5ab629",
+    "8": "#ad1c6b",
+    "9": "#000000",
+    ",": "#000000",
+    ".": "#000000",
+}
+
+export default {
+    props: {
+        character: String
+    },
+    computed: {
+        color () {
+            return dictionary[this.character]
+        }
+    }
+}
+</script>
+
+<style>
+span {
+font-weight: bold;
+font-size: 4rem;
+word-break: break-all;
+text-shadow: 1px 1px 1px #000;
+
+}
+</style>
